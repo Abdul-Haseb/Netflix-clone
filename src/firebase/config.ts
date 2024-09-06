9438d406465fbc3a28bb67e0f829701694657bd2
Haseeb <zed.h8050@gmail.com>
2024-09-06 17:55:41 +0500

    Api key set

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,23 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const {
-  VITE_FIRBASE_API_KEY,
-  VITE_FIRBASE_AUTH_DOMAIN,
-  VITE_FIRBASE_PROJECT_ID,
-  VITE_FIRBASE_STORAGE_BUCKET,
-  VITE_FIRBASE_MESAGING_SENDER_ID,
-  VITE_FIRBASE_APP_ID,
-} = import.meta.env;
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: VITE_FIRBASE_API_KEY,
-  authDomain: VITE_FIRBASE_AUTH_DOMAIN,
-  projectId: VITE_FIRBASE_PROJECT_ID,
-  storageBucket: VITE_FIRBASE_STORAGE_BUCKET,
-  messagingSenderId: VITE_FIRBASE_MESAGING_SENDER_ID,
-  appId: VITE_FIRBASE_APP_ID,
+  apiKey: "AIzaSyBFS4qpSEM29Y_qgwXHa-tWXkgsCVO-PAc",
+  authDomain: "netlify-6f701.firebaseapp.com",
+  projectId: "netlify-6f701",
+  storageBucket: "netlify-6f701.appspot.com",
+  messagingSenderId: "175046071160",
+  appId: "1:175046071160:web:bfbe72e5e34b4c920dae8d",
 };
 
 // Initialize Firebase
